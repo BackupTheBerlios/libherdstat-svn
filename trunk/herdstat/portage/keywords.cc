fac29ee2f63295192b50900182f9d4b83914a659
ka0ttic <ka0ttic@830c426f-6502-0410-a408-9daea7eb3ada>
2005-10-10 12:36:21 +0000

    Added initial implementation of Archs/Keyword/Keywords classes.

---
+++ trunk/herdstat/portage/keywords.cc
@@ -0,0 +1,187 @@
+/*
+ * libherdstat -- herdstat/portage/keywords.cc
+ * $Id$
+ * Copyright (c) 2005 Aaron Walker <ka0ttic@gentoo.org>
+ *
+ * This file is part of libherdstat.
+ *
+ * libherdstat is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * libherdstat is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * libherdstat; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 325, Boston, MA  02111-1257  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <herdstat/util/misc.hh>
+#include <herdstat/util/string.hh>
+#include <herdstat/portage/exceptions.hh>
+#include <herdstat/portage/keywords.hh>
+
+namespace herdstat {
+namespace portage {
+/*** static members *********************************************************/
+const Archs Keyword::_valid_archs;
+const std::string Keyword::maskc::_valid_masks("-~");
+/****************************************************************************/
+Keyword::maskc::maskc(const char c) : _c(0)
+{
+    if (_valid_masks.find(c) == std::string::npos)
+        throw InvalidKeywordMask(c);
+
+    _c = c;
+}
+/****************************************************************************/
+bool
+Keyword::maskc::operator< (const maskc& that) const
+{
+    if (_c == that._c)
+        return false;
+
+    /* '-' is less than everything */
+    if (_c == '-')
+        return false;
+
+    if (_c == '~' and that._c == '-')
+        return false;
+
+    return true;
+}
+/****************************************************************************/
+Keyword::Keyword(const std::string& kw)
+{
+    this->parse(kw);
+
+    if (not _valid_archs.count(_arch))
+        throw InvalidArch(_arch);
+}
+/****************************************************************************/
+void
+Keyword::parse(const std::string& kw)
+{
+    if (kw[0] == '-' or kw[0] == '~')
+    {
+        _mask = kw[0];
+        _arch = kw.substr(1);
+    }
+    else
+        _arch = kw;
+}
+/****************************************************************************/
+Keywords::Keywords(bool use_colors)
+    : _color(use_colors), _ebuild(), _str()
+{
+}
+/****************************************************************************/
+Keywords::Keywords(const std::string& path, bool use_colors)
+    : _color(use_colors), _ebuild(path), _str()
+{
+    this->fill();
+    this->format();
+}
+/****************************************************************************/
+Keywords::Keywords(const ebuild& e, bool use_colors)
+    : _color(use_colors), _ebuild(e), _str()
+{
+    this->fill();
+    this->format();
+}
+/****************************************************************************/
+void
+Keywords::assign(const std::string& path, bool use_colors)
+{
+    _color = use_colors;
+    _ebuild.read(path);
+    this->fill();
+    this->format();
+}
+/****************************************************************************/
+void
+Keywords::assign(const ebuild& e, bool use_colors)
+{
+    _color = use_colors;
+    _ebuild = e;
+    this->fill();
+    this->format();
+}
+/****************************************************************************/
+
+    /**
+     * @struct KeywordLess
+     * @brief Determine if the first keyword is less than the second keyword.
+     */
+//    struct KeywordLess
+//    {
+//        bool operator()(const std::string& kw1, const std::string& kw2) const
+//        {
+//            /* '-*' is less than everything */
+//            if (kw1 == "-*")
+//                return true;
+
+//            const char k1 = kw1.at(0);
+//            const char k2 = kw2.at(0);
+
+//            /* if first char of both is equal or the first char of both
+//             * does not equal a mask char, use normal string comparison */
+//            if ((k1 == k2) or
+//                (k1 != '-' and k1 != '~' and k2 != '-' and k2 != '~'))
+//                return (kw1 < kw2);
+
+//            if (k1 == '-')
+//                return true;
+//            if (k1 == '~' and k2 != '-')
+//                return true;
+
+//            return false;
+//        }
+//    };
+
+void
+Keywords::fill()
+{
+    if (_ebuild["KEYWORDS"].empty())
+        throw Exception(_ebuild.path()+": no KEYWORDS variable defined");
+
+    const std::vector<std::string> v(util::split(_ebuild["KEYWORDS"]));
+    std::vector<std::string>::const_iterator i;
+    for (i = v.begin() ; i != v.end() ; ++i)
+        _keywords.insert(Keyword(*i));
+}
+
+void
+Keywords::format()
+{
+    util::ColorMap cmap;
+    size_type n = 1;
+    for (iterator i = _keywords.begin() ; i != _keywords.end() ; ++i, ++n)
+    {
+        if (_color)
+        {
+            if (i->mask() == '-')        _str += cmap[red];
+            else if (i->mask() == '~')   _str += cmap[yellow];
+            else                         _str += cmap[blue];
+            _str += i->str() + cmap[none];
+        }
+        else
+            _str += i->str();
+
+        if ((n+1) != _keywords.size())
+            _str += " ";
+    }
+}
+/****************************************************************************/
+} // namespace portage
+} // namespace herdstat
+
+/* vim: set tw=80 sw=4 et : */
---
+++ trunk/herdstat/portage/keywords.hh
@@ -0,0 +1,174 @@
+/*
+ * libherdstat -- herdstat/portage/keywords.hh
+ * $Id$
+ * Copyright (c) 2005 Aaron Walker <ka0ttic@gentoo.org>
+ *
+ * This file is part of libherdstat.
+ *
+ * libherdstat is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * libherdstat is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * libherdstat; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 325, Boston, MA  02111-1257  USA
+ */
+
+#ifndef _HAVE_KEYWORDS_HH
+#define _HAVE_KEYWORDS_HH 1
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+/**
+ * @file keywords.hh
+ * @brief Defines the Keyword/Keywords classes.
+ */
+
+#include <string>
+#include <herdstat/util/container_base.hh>
+#include <herdstat/portage/ebuild.hh>
+#include <herdstat/portage/archs.hh>
+
+namespace herdstat {
+namespace portage {
+
+    /**
+     * @class Keyword
+     * @brief Represents a single architecture keyword.
+     */
+    class Keyword
+    {
+        public:
+            Keyword(const std::string& kw);
+
+            const char mask() const { return _mask; }
+            const std::string& arch() const { return _arch; }
+            const std::string str() const
+            { std::string s(1, _mask); s+=_arch; return s; }
+
+            bool operator< (const Keyword& that) const
+            { return ((_mask < that._mask) and (_arch < that._arch)); }
+            bool operator> (const Keyword& that) const
+            { return (that < *this); }
+            bool operator==(const Keyword& that) const
+            { return ((_mask == that._mask) and (_arch == that._arch)); }
+            bool operator!=(const Keyword& that) const
+            { return (*this != that); }
+
+        private:
+            /**
+             * @class maskc
+             * @brief Keyword mask character ('~' for example).
+             */
+            class maskc
+            {
+                public:
+                    maskc() : _c(0) { }
+                    maskc(const char c);
+                    maskc& operator= (const char c)
+                    { _c = c; return *this; }
+                    
+                    /// implicit conversion to char
+                    operator char() const { return _c; }
+
+                    bool empty() const { return (_c != 0); }
+
+                    bool operator< (const maskc& that) const;
+                    bool operator> (const maskc& that) const
+                    { return (that < *this); }
+                    bool operator==(const maskc& that) const
+                    { return (_c == that._c); }
+                    bool operator!=(const maskc& that) const
+                    { return (*this != that); }
+
+                private:
+                    char _c;
+                    static const std::string _valid_masks;
+            };
+
+
+            /// parse keyword.
+            void parse(const std::string& kw);
+
+            std::string _arch;
+            maskc _mask;
+            static const Archs _valid_archs;
+    };
+
+    /**
+     * @class Keywords
+     * @brief Keyword container.
+     * Usually used for all keywords for a certain ebuild.
+     * Also able to provide a pretty-printed keywords string.
+     */
+
+    class Keywords : public util::SetBase<Keyword>
+    {
+        public:
+            /** Default constructor.
+             * @param use_colors Use colors when formatting keywords string.
+             */
+            Keywords(bool use_colors = false);
+
+            /** Constructor.
+             * @param path Path to ebuild.
+             * @param use_colors Use colors when formatting keywords string.
+             */
+            Keywords(const std::string& path, bool use_colors = false);
+
+            /** Constructor.
+             * @param e Pre-existing ebuild instance.
+             * @param use_colors Use colors when formatting keywords string.
+             */
+            Keywords(const ebuild& e, bool use_colors = false);
+
+            /** Assign new ebuild path.
+             * @param path Path to ebuild.
+             * @param use_colors Use colors when formatting keywords string.
+             */
+            void assign(const std::string& path, bool use_colors = false);
+
+            /** Assign new ebuild.
+             * @param e Pre-existing ebuild instance.
+             * @param use_colors Use colors when formatting keywords string.
+             */
+            void assign(const ebuild& e, bool use_colors = false);
+
+            /// Get formatted keywords string.
+            inline const std::string& str() const;
+
+            /// Get path to ebuild associated with these keywords.
+            inline const std::string& path() const;
+
+            /// Are all keywords masked?
+//            bool all_masked() const;
+            /// Are all ebuilds unstable?
+//            bool all_unstable() const;
+
+        private:
+            void fill();
+            /// prepare keywords string
+            void format();
+
+            bool _color;
+            ebuild _ebuild;
+            std::string _str;
+    };
+
+    inline const std::string& Keywords::str() const { return _str; }
+    inline const std::string& Keywords::path() const { return _ebuild.path(); }
+
+} // namespace portage
+} // namespace herdstat
+
+#endif /* _HAVE_KEYWORDS_HH */
+
+/* vim: set tw=80 sw=4 et : */
